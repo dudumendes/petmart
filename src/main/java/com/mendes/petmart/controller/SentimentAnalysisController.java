ec6508d7fd130d4ed7148ac7cac16f7a06753570
Eduardo Mendes <edumendes@gmail.com>
2025-06-10 16:09:40 -0400

    Simple sentiment analysis

--- src/main/java/com/mendes/petmart/controller/SentimentAnalysisController.java
+++ src/main/java/com/mendes/petmart/controller/SentimentAnalysisController.java
@@ -1,9 +1,30 @@
 package com.mendes.petmart.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("anasl")
-public class SentimentAnalisysController {
+@RequestMapping("analysis")
+public class SentimentAnalysisController {
+    private final ChatClient chatClient;
+
+    public SentimentAnalysisController(
+            ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping
+    public String analysis() {
+        String review = "This log is dirty";
+
+        var prompt = String.format(
+                "Analyze the sentiment of the following review: %s. Respond with 'Positive', 'Negative' or 'Neutral'.", review);
+
+        return this.chatClient.prompt()
+                .user(prompt)
+                .call()
+                .content();
+    }
 }
