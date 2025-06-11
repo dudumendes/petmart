d0629c4de27d95bf50bd0c72793252d653f0041e
Eduardo Mendes <edumendes@gmail.com>
2025-06-11 10:56:31 -0400

    class to use jtokkit

--- src/main/java/com/mendes/petmart/model/Tokenizer.java
+++ src/main/java/com/mendes/petmart/model/Tokenizer.java
@@ -1,4 +1,12 @@
 package com.mendes.petmart.model;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
+
 public class Tokenizer {
+    public static int countTokens(String system, String user) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_4O_MINI);
+        return enc.countTokens(system + user);
+    }
 }
